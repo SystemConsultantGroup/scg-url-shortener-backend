f6e9f5acd1c49cd5f2f8857e5bea98e4f5e3285e
krong12 <jinkim0602@gmail.com>
2026-01-13 17:52:43 +0900

    feat: User, URL 엔티티 추가
    * rebase 수행

---
+++ src/main/java/com/example/demo/entity/User.java
@@ -0,0 +1,27 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@NoArgsConstructor
+@Getter
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String nickname;
+
+    @CreationTimestamp
+    @Column(updatable = false, nullable = false)
+    private LocalDateTime created_at;
+}
