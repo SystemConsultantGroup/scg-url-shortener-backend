9a4550f7f6dc31c6175f283d2bceef7fc243d346
hyzoon <shj83811081@gmail.com>
2026-01-14 00:03:25 +0900

    refactor: package name

--- src/main/java/com/scg/shortener/entity/URL.java
+++ src/main/java/com/scg/shortener/entity/URL.java
@@ -1,27 +1,35 @@
-package com.example.demo.entity;
+package com.scg.shortener.entity;
 
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import java.time.LocalDateTime;
 
 @Entity
 @NoArgsConstructor
 @Getter
-public class User {
+public class URL {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
-    @Column(nullable = false)
-    private String email;
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(unique = true, nullable = false)
+    private String slug;
 
     @Column(nullable = false)
-    private String nickname;
+    private String target_url;
 
     @CreationTimestamp
     @Column(updatable = false, nullable = false)
     private LocalDateTime created_at;
+
+    @UpdateTimestamp
+    private LocalDateTime updated_at;
 }
--- src/main/java/com/scg/shortener/entity/User.java
+++ src/main/java/com/scg/shortener/entity/User.java
@@ -1,35 +1,27 @@
-package com.example.demo.entity;
+package com.scg.shortener.entity;
 
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.annotations.UpdateTimestamp;
 
 import java.time.LocalDateTime;
 
 @Entity
 @NoArgsConstructor
 @Getter
-public class URL {
+public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
-    @ManyToOne
-    @JoinColumn(name = "user_id", nullable = false)
-    private User user;
-
-    @Column(unique = true, nullable = false)
-    private String slug;
+    @Column(nullable = false)
+    private String email;
 
     @Column(nullable = false)
-    private String target_url;
+    private String nickname;
 
     @CreationTimestamp
     @Column(updatable = false, nullable = false)
     private LocalDateTime created_at;
-
-    @UpdateTimestamp
-    private LocalDateTime updated_at;
 }
