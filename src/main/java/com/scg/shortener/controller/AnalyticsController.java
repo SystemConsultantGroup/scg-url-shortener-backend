754e3c2ca10962994ce5bfa5bc0bfd062d07d435
apersomany <aperso@aperso.dev>
2026-02-05 16:38:25 +0900

    implement auth in analytics

--- src/main/java/com/scg/shortener/controller/AnalyticsController.java
+++ src/main/java/com/scg/shortener/controller/AnalyticsController.java
@@ -5,8 +5,12 @@ import com.scg.shortener.entity.UrlMapping;
 import com.scg.shortener.repository.UrlMappingRepositry;
 import com.scg.shortener.service.AnalyticsService;
 
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,21 +21,27 @@ import java.security.Principal;
 @RestController
 @RequiredArgsConstructor
 public class AnalyticsController {
-    private final AnalyticsService analyticsService;
     private final UrlMappingRepositry urlMappingRepositry;
+    private final AnalyticsService analyticsService;
 
+    @Transactional
     @GetMapping("/urls/{slug}/analytics")
     public AnalyticsResponse getAnalytics(
             @PathVariable String slug,
             Principal principal) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication == null) {
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED);
+        }
+        String username = null;
+        if (authentication.getPrincipal() instanceof UserDetails details) {
+            username = details.getUsername();
+        }
         UrlMapping urlMapping = urlMappingRepositry.findBySlug(slug)
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "URL not found"));
-
-        // if (principal == null ||
-        // !urlMapping.getUser().getEmail().equals(principal.getName())) {
-        // throw new ResponseStatusException(HttpStatus.FORBIDDEN, "Forbidden");
-        // }
-
+        if (!urlMapping.getUser().getEmail().equals(username)) {
+            throw new ResponseStatusException(HttpStatus.FORBIDDEN, "Forbidden");
+        }
         return analyticsService.getAnalyticsResponse(slug);
     }
 }
